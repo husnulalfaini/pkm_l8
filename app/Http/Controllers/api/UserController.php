a3d630a014970b17e429c0209449d3f216f41326
Husnul Alfaini <husnulalfaini3@gmail.com>
2021-09-14 15:31:08 +0700

    membuat upload gambar pada function update data api user controller

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Image;
 use Validator;
 use App\Models\User;
 
@@ -57,13 +59,31 @@ class UserController extends Controller
     
         public function update(Request $request)
     {
-
+        
+        $image = $request->file('foto')->getClientOriginalName();
+        $request->file('foto')->move('public/storage', $image);
+          // your base64 encoded
+        // $image = $request->foto;  // your base64 encoded
+        //          $imageName =  $request->get('nama').time().'.jpeg';
+        //          \File::put(public_path('storage/') . $imageName, ($image));
+                 
         $user = User::where('id','=',$request->id)->first();
         $user->id   = $request->id;
         $user->name   = $request->name;
         $user->email   = $request->email;
         $user->alamat   = $request->alamat;
         $user->telepon  = $request->telepon;
+        $user->foto        = $image;
+
+        // $new_foto = $request->file('foto');
+        // if($new_foto){
+        //     if($user->foto && file_exists(storage_path('app/public/uploads/' .$user->foto))){
+        //             Storage::delete('public/uploads/'. $user->foto);
+        //             }
+        //     $images = 'user_photobaru'.time().'.'.$request->file('foto')->extension();
+        //     Image::make($new_foto)->resize(300, 300)->save(storage_path('app/public/uploads/' . $images));
+        //     $user->foto = $images;
+        // }
         $user->save();
 
         return response()->json($user, 201);
